d67f31f0168563fd5cc895308e99ba4d1247e540
Carisse Barr <carissebarr.swe@gmail.com>
2023-05-09 22:13:56 -0400

    updated contact component page

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "";
+
+function ContactPage() {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default ContactPage;
